816f39f1b808554d400ecd340074d664186d1baf
Trevor Seibert <trevorpaulseibert@gmail.com>
2022-04-07 11:02:58 -0500

    Cleaned Data to specific veriables

---
+++ the_code/linear_regression.py
@@ -0,0 +1,7 @@
+import pandas as pd
+import numpy as np
+from pandas_profiling import ProfileReport
+import statsmodels.api as smf
+import sklearn as sk 
+import scipy as sp
+import matplotlib as plt
